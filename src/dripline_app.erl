cbea97bf1435c1e6e411e411ff86bb055ef1433a
Jared Kofron <jared.kofron@gmail.com>
2011-04-28 09:25:25 -0700

    first commit of src dir, all rebar files

---
+++ src/dripline_app.erl
@@ -0,0 +1,16 @@
+-module(dripline_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+start(_StartType, _StartArgs) ->
+    dripline_sup:start_link().
+
+stop(_State) ->
+    ok.
