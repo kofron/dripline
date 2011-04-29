8a5fccb3eed35330383d48636dba888e94ef16fb
Jared Kofron <jared.kofron@gmail.com>
2011-04-29 13:20:31 -0700

    now builds correctly, untested yet

--- priv/dripline_backplane.c
+++ priv/dripline_backplane.c
@@ -10,20 +10,40 @@
 struct backplane {
   int handle;  
 };
+typedef struct backplane backplane;
+static ErlNifResourceType* backplane_rsrc;
 
 static ERL_NIF_TERM start(ErlNifEnv* env, 
 			  int argc, 
 			  const ERL_NIF_TERM argv[])
 {
-  struct backplane* bp_handle = (struct backplane*)erl_nif_alloc(struct backplane, 
-						   sizeof(backplane));
+  backplane* bp_handle = enif_alloc_resource(backplane_rsrc,
+					     sizeof(backplane));
   ERL_NIF_TERM ret = enif_make_resource(env,bp_handle);
   enif_release_resource(bp_handle);
   return enif_make_tuple2(env,enif_make_atom(env,"ok"),ret);
+}
+
+static void unload_cleanup(ErlNifEnv* env, void* arg)
+{
+}
+
+static int load_init(ErlNifEnv* env, 
+		     void** priv_data, 
+		     ERL_NIF_TERM load_info)
+{
+  backplane_rsrc = enif_open_resource_type(env,
+					   "dripline_backplane",
+					   "backplane_rsrc",
+					   &unload_cleanup,
+					   ERL_NIF_RT_CREATE | ERL_NIF_RT_TAKEOVER,
+					   0);
+  return 0;
 }
 
 static ErlNifFunc backplane_funcs[] = {
   {"start",0,start}
 };
 
-ERL_NIF_INIT(dripline_backplane,backplane_funcs,NULL,NULL,NULL,NULL)
+ERL_NIF_INIT(dripline_backplane,backplane_funcs,&load_init,
+	     NULL,NULL,NULL)
