16d74b2e7bc29e0cc91ab339ee713dfaa1bd78a1
Jared Kofron <jared.kofron@gmail.com>
2012-02-29 20:01:22 -0800

    first attempt at updating document in place with result.

--- src/dripline_dispatch.erl
+++ src/dripline_dispatch.erl
@@ -14,20 +14,22 @@ dispatch_doc(CmdDoc) ->
 			Conn = dripline_conn_mgr:get(),
 			{ok, Db} = couchbeam:open_db(Conn,"dripline_conf"),
 			{ok, InstrDoc} = couchbeam:open_doc(Db,DocId),
-			dispatch_instr(CmdDoc,InstrDoc)
+			dispatch_instr(Db,CmdDoc,InstrDoc)
 	end.
 
-dispatch_instr(CmdDoc, InstrDoc) ->
+dispatch_instr(Db,CmdDoc, InstrDoc) ->
 	Module = couchbeam_doc:get_value(<<"instrument_model">>,InstrDoc),
 	CallMod = binary_to_atom(Module),
 	InstrId = couchbeam_doc:get_value(<<"instrument_id">>,InstrDoc),
 	CallId = binary_to_atom(InstrId),
 	{Func,Args} = parse_f_a(CmdDoc),
 	Result = CallMod:Func(CallId,Args),
-	update_doc(CmdDoc, Result).
+	update_doc(Db, CmdDoc, Result).
 
-update_doc(CmdDoc, CmdResult) ->
-	ok.
+update_doc(Db, CmdDoc, CmdResult) ->
+	UpDoc = couchbeam_doc:set_value(<<"result">>,CmdResult,CmdDoc),
+	couchbeam:save_doc(Db,UpDoc),
+	io:format("~p~n",[CmdResult]).
 
 parse_f_a(CmdDoc) ->
 	io:format("~p~n",[CmdDoc]),
