166e5c3fa4e87dc8f402293c37e7c70bc089f071
Jared Kofron <jared.kofron@gmail.com>
2012-02-29 16:13:04 -0800

    starts processes for both of the agilent mux units that we have.

--- src/dripline_instr_sup.erl
+++ src/dripline_instr_sup.erl
@@ -24,4 +24,28 @@ start_link() ->
 
 init([]) ->
     SuperStrategy = {one_for_one, 5, 10},
-    {ok, { SuperStrategy, [] }}.
+    SwitchMux = {
+		switch_mux,
+		{
+			agilent34970a,
+			start_link,
+			[switch_mux,left_box,8]
+		},
+		permanent,
+		5000,
+		worker,
+		[agilent34790a]
+	},
+	ADCMux = {
+		adc_mux,
+		{
+			agilent34970a,
+			start_link,
+			[adc_mux,left_box,9]
+		},
+		permanent,
+		5000,
+		worker,
+		[agilent34790a]
+	},
+    {ok, { SuperStrategy, [SwitchMux,ADCMux] }}.
