b75c54bf1d9777f9176a91ba497601b765d979a8
Jared Kofron <jared.kofron@gmail.com>
2014-04-29 13:26:04 -0700

    moved to gen_gpib_spoller behavior

--- apps/dl_core/src/instruments/agilent_e3631a.erl
+++ apps/dl_core/src/instruments/agilent_e3631a.erl
@@ -1,143 +1,70 @@
-%% @doc The Agilent E3631A is a triple output laboratory power supply.
-%% @author jared kofron <jared.kofron@gmail.com>
-%% @version 0.1a
+%%%-------------------------------------------------------------------
+%%% @author Jared Kofron <jared.kofron@gmail.com>
+%%% @copyright (C) 2014, Jared Kofron
+%%% @doc
+%%%
+%%% @end
+%%% Created : 10 Feb 2014 by Jared Kofron <jared.kofron@gmail.com>
+%%%-------------------------------------------------------------------
 -module(agilent_e3631a).
--behavior(gen_prologix).
+-behavior(gen_gpib_spoller).
 
-%%%%%%%%%%%
-%%% API %%%
-%%%%%%%%%%%
--export([handle_get/2, handle_set/3]).
+-export([start_link/4,
+	 init/1,
+	 handle_stb/2,
+	 handle_esr/2,
+	 handle_get/2,
+	 handle_set/3,
+	 sre_register_bitmask/1,
+	 ese_register_bitmask/1
+	]).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% gen_server api and callbacks %%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
--export([init/1,start_link/3]).
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% internal record defs %%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -record(state, {}).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%% gen_server api and callbacks definitions %%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-start_link(InstrumentID,EProID,GPIBAddress) ->
-    gen_prologix:start_link(?MODULE, InstrumentID, EProID, GPIBAddress).
+-define(srq_asserted(X), ((X bsr 6) band 1) == 1).
+-define(mav_asserted(X), ((X bsr 4) band 1) == 1).
 
-init(_Args) ->
-    InitialState = #state{},
-    {ok, InitialState}.
+sre_register_bitmask(_) ->
+    176.
+ese_register_bitmask(_) ->
+    61.
 
-handle_get('6V', StateData) ->
-    {send, <<"MEAS:VOLT? P6V;:MEAS:CURR? P6V">>, StateData};
-handle_get('25V+', StateData) ->
-    {send, <<"MEAS:VOLT? P25V;:MEAS:CURR? P25V">>, StateData};
-handle_get('25V-', StateData) ->
-    {send, <<"MEAS:VOLT? N25V;:MEAS:CURR? N25V">>, StateData}.
+init([]) ->
+    {ok, #state{}}.
 
-handle_set('6V', NewValue, StateData) ->
-    Branch = case unpack_value(NewValue) of
-		 {ok, <<"ON">>} ->
-		     ToSend = ["INST:SEL P6V;:","OUTP:STAT ON"],
-		     {send, ToSend, StateData};
-		 {ok, <<"OFF">>} ->
-		     ToSend = ["INST:SEL P6V;:","OUTP:STAT OFF"],
-		     {send, ToSend, StateData};
-		 {ok, Parsed} ->
-		     ToSend = ["APPL P6V,",Parsed],
-		     {send, ToSend, StateData};
-		 {error, Reason} ->
-		     {error, Reason, StateData}
-	     end,
-    Branch;
-handle_set('25V+', NewValue, StateData) ->
-    Branch = case unpack_value(NewValue) of
-		 {ok, <<"ON">>} ->
-		     ToSend = ["INST:SEL P25V;:","OUTP:STAT ON"],
-		     {send, ToSend, StateData};
-		 {ok, <<"OFF">>} ->
-		     ToSend = ["INST:SEL P25V;:","OUTP:STAT OFF"],
-		     {send, ToSend, StateData};
-		 {ok, Parsed} ->
-		     ToSend = ["APPL P25V,",Parsed],
-		     {send, ToSend, StateData};
-		 {error, Reason} ->
-		     {error, Reason, StateData}
-	     end,
-    Branch;
-handle_set('25V-', NewValue, StateData) ->
-    Branch = case unpack_value(NewValue) of
-		 {ok, <<"ON">>} ->
-		     ToSend = ["INST:SEL N25V;:","OUTP:STAT ON"],
-		     {send, ToSend, StateData};
-		 {ok, <<"OFF">>} ->
-		     ToSend = ["INST:SEL N25V;:","OUTP:STAT OFF"],
-		     {send, ToSend, StateData};
-		 {ok, Parsed} ->
-		     ToSend = ["APPL N25V,",Parsed],
-		     {send, ToSend, StateData};
-		 {error, Reason} ->
-		     {error, Reason, StateData}
-	     end,
-    Branch.
+start_link(InstrName, GPIBAddress, BusMod, BusName) ->
+    gen_gpib_spoller:start_link(?MODULE, InstrName, GPIBAddress, BusMod, BusName).
 
-%%---------------------------------------------------------------------%%
-%% @doc unpack_value converts a value passed in from the database 
-%%---------------------------------------------------------------------%%
--spec unpack_value(binary()) -> 
-			  {ok, binary()} | {error, term()}.
-unpack_value(<<"enable">>) ->
-    {ok, <<"ON">>};
-unpack_value(<<"disable">>) ->
-    {ok, <<"OFF">>};
-unpack_value(Bin) ->
-    case binary:match(Bin,<<",">>) of
-	nomatch ->
-	    parse_const_mode(Bin);
-	_Match ->
-	    parse_reg_mode(Bin)
-    end.
-	    
--spec parse_const_mode(binary()) -> binary().
-parse_const_mode(B) ->
-    case {is_v(B), is_c(B)} of
-	{true, false} ->
-	    {ok, [drop_v(B),",",<<"MAX">>]};
-	{false, true} ->
-	    {ok, [<<"MAX">>,",",drop_c(B)]};
-        _AnyOther ->
-	    {error, {badval, B}}
+handle_get(Any, StateData) ->
+    {stop, unimplemented, StateData}.
+
+handle_set(Any, Value, StateData) ->
+    {stop, unimplemented, StateData}.
+
+%% 
+%% If there is a message available, that is the highest priority. 
+%% Otherwise, we retrieve errors or clear the status byte as necessary.
+%%
+handle_stb(StatusByte, StateData) when ?srq_asserted(StatusByte) ->
+    case StatusByte of
+	_MsgAvail when ?mav_asserted(StatusByte) ->
+	    {retrieve_data, StateData};
+	Err when Err =:= 224; Err =:= 144; Err =:= 192; Err =:= 208 ->
+	    {retrieve_error, <<"err?">>, StateData};
+	ESR when ESR =:= 96; ESR =:= 112 ->
+	    {fetch_esr, <<"*esr?">>, StateData}
+    end;
+handle_stb(StatusByte, StateData) ->
+    case StatusByte of
+	Err when Err =:= 160; Err =:= 80; Err =:= 128; Err =:= 144 ->
+	    {retrieve_error, <<"err?">>, StateData};
+	ESR when ESR =:= 32; ESR =:= 48 ->
+	    {fetch_esr, <<"*esr?">>, StateData};
+	_MsgAvail when ?mav_asserted(StatusByte) ->
+	    {retrieve_data, StateData}
     end.
 
--spec parse_reg_mode(binary()) -> binary().
-parse_reg_mode(B) ->
-    [V,C] = binary:split(B,<<",">>),
-    case {is_v(V), is_c(V), is_v(C), is_c(C)} of
-	{true, false, false, true} -> % Voltage, Current
-	    {ok, [drop_v(V),",",drop_c(C)]};
-	{false, true, false, true} -> % Current, Current
-	    {error, {badval, B}};
-	{false, true, true, false} -> % Current, Voltage
-	    {ok, [drop_v(C),",",drop_c(V)]};
-	{true, false, true, false} -> % Voltage, Voltage
-	    {error, {badval, B}} ;
-	_Other -> % Nonsense
-	    {error, {badval,B}}
-    end.
-
--spec is_v(binary()) -> boolean().
-is_v(B) ->
-    binary:last(B) == $V.
-
--spec is_c(binary()) -> boolean().
-is_c(B) ->
-    binary:last(B) == $A.
-
--spec drop_v(binary()) -> binary().
-drop_v(Bin) ->
-    binary:replace(Bin,<<"V">>,<<>>).
-
--spec drop_c(binary()) -> binary().
-drop_c(Bin) ->
-    binary:replace(Bin,<<"A">>,<<>>).
+handle_esr(1, StateData) ->
+    {op_complete, StateData};
+handle_esr(Err, StateData) when Err =:= 8; Err =:= 33; Err =:= 9; Err =:= 32 ->
+    {retrieve_error, <<"syst:err?">>, StateData}.
